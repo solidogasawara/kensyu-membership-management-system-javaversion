7beb8d8665ab14d87a4d007054d2d54030516af7
solidogasawara <t.ogasawara@solidseed.co.jp>
2023-08-14 17:23:01 +0900

    update: DBログイン関連の情報を別ファイルに保存し、そこから読み込む仕様に変更

--- src/main/java/model/DBManager.java
+++ src/main/java/model/DBManager.java
@@ -3,14 +3,31 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class DBManager {
-	private static String URL = "jdbc:mysql://localhost:3306/";
-	private static String DB_NAME = "customers";
-	private static String USER_NAME = "root";
-	private static String PASSWORD = "root";
+	
+	private static String URL = "";
+	private static String DB_NAME = "";
+	private static String USER_NAME = "";
+	private static String PASSWORD = "";
+	
+	private static void loadPropertiesFile() {
+		try {
+			ResourceBundle rb = ResourceBundle.getBundle("/properties/DBAccess");
+			
+			URL = rb.getString("url");
+			DB_NAME = rb.getString("db_name");
+			USER_NAME = rb.getString("user_name");
+			PASSWORD = rb.getString("password");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static Connection getConnection() throws SQLException {
+		loadPropertiesFile();
+		
 		String accessUrl = URL + DB_NAME + "?";
 		
 		Connection con = DriverManager.getConnection(accessUrl, USER_NAME, PASSWORD);
